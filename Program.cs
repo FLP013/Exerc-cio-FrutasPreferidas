4ea7299785789fea8b051691262df0476069b83a
FLP013 <felliperibeiro9@gmail.com>
2024-09-02 21:51:27 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Minhas Frutas Preferidas ---");
+
+Console.ForegroundColor = ConsoleColor.Yellow; 
+Console.WriteLine("BANANA");
+Console.ForegroundColor = ConsoleColor.Blue; 
+Console.WriteLine("MIRTILO");
+Console.ForegroundColor = ConsoleColor.Green; 
+Console.WriteLine("LIMÃO");
+Console.ForegroundColor = ConsoleColor.Magenta; 
+Console.WriteLine("PITAYA");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("MORANGO");
+Console.ResetColor();
